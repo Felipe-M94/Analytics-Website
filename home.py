63da875e20dd71f1658deb472ee25b0bf3a8cc01
Felipe-M94 <felipe.silva.mendonca@gmail.com>
2023-12-05 17:37:25 -0300

    Configs Streamlit

---
+++ home.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+from streamlit_option_menu import option_menu
+from numerize.numerize import numerize
+
+st.set_page_config(page_title="Dasboard", page_icon="🌎", layout="wide")
+st.subheader("🔔 Insurance Descriptive Analytics")
+st.markdown("##")
+
+
